8876e5b593e8b5b91daad6c6cfd6f51fb6ad925f
thelissimus <thelissimus@tuta.io>
2026-08-02 03:08:59 +0500

    define: `struct Chip8`

--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
+#include <stdint.h>
+
+struct Chip8 {
+	uint8_t registers[16];
+	uint8_t memory[4096];
+	uint16_t index;
+	uint16_t pc;
+	uint16_t stack[16];
+	uint8_t sp;
+	uint8_t delay_timer;
+	uint8_t sound_timer;
+	uint8_t keypad[16];
+	uint8_t video[32][64];
+};
+
 int
 main(void)
 {
